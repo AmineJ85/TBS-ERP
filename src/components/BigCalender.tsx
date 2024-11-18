0a736d5a12bc3f3d258870b7771efc59f3fd6d54
Amine Jebri <amine.jabri@tbs.u-tunis.tn>
2024-11-18 13:30:48 +0100

    V0.1

---
+++ src/components/BigCalender.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
+import moment from "moment";
+import { calendarEvents } from "@/lib/data";
+import { useState } from "react";
+
+const localizer = momentLocalizer(moment);
+
+const BigCalendar = () => {
+  const [view, setView] = useState<View>(Views.WEEK);
+  const handleOnchangeView = (selectedView: View) => {
+    setView(selectedView);
+  };
+  return (
+    <Calendar
+      localizer={localizer}
+      events={calendarEvents}
+      startAccessor="start"
+      endAccessor="end"
+      views={["week", "day", "agenda"]}
+      view={view}
+      style={{ height: "98%" }}
+      onView={handleOnchangeView}
+      min={new Date(2025, 1, 0, 8, 0, 0)}
+      max={new Date(2025, 1, 0, 18, 0, 0)}
+    />
+  );
+};
+export default BigCalendar;
